52741288f93208fad410468d8deee85e657e51ca
GOARANT Dylan <d.goarant@rns.ecolescreatives.com>
2022-10-18 15:23:46 +0200

    Set

---
+++ Assets/Dylan/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Dylan
+{
+    public class Spawner : MonoBehaviour
+    {
+        public GameObject objectToSpawn;
+        public Transform parent;
+        public GameObject Rouge;
+        public GameObject Bleu;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
