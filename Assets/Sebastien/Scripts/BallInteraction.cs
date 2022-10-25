985ddf2b203bfdd5097653448eb99c5d9efd516d
ssalmon35 <s.salmon@rns.ecolescreatives.com>
2022-10-25 13:15:51 +0200

    Mini-Jeu 1 - Bases gameplay

--- Assets/Sebastien/Scripts/BallInteraction.cs
+++ Assets/Sebastien/Scripts/BallInteraction.cs
@@ -1,38 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class BallInteraction : MonoBehaviour
 {
     public KeyCode mainKey;
     public float strength;
-    Rigidbody2D rb;
+    public float strengthMax = 10f;
 
+    Rigidbody2D rb;
+    public Scrollbar scrollbar;
     public bool isThrowing = false;
 
     // Start is called before the first frame update
     void Start()
     {
+
         rb = GetComponent<Rigidbody2D>();
-        strength = 2f;
+        strength = 1f;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKeyDown(mainKey))
-        //{
-        //    //rb.AddForce(transform.right * strength, ForceMode2D.Impulse);
-        //    //rb.AddForce(transform.up * strength, ForceMode2D.Impulse);
 
-        //}
+        // Strength minimum = 0
+        // Strength maximum (capée pour ne pas envoyer la balle trop loin) = 8
 
-        if (Input.GetKey(mainKey) && isThrowing == false)
+        if (Input.GetKey(mainKey) && isThrowing == false && strength < 10f)
         {
 
             strength += 0.1f;
+            
 
         }
+
+        else if (Input.GetKey(mainKey) && isThrowing == false && strength >= 10f)
+        {
+
+            strength -= 0.1f;
+
+
+        }
+
 
 
         else if (Input.GetKeyUp(mainKey) && isThrowing == false)
@@ -45,7 +58,21 @@ public class BallInteraction : MonoBehaviour
         }
 
 
+        if (strength > 10f)
+        {
+            strength = 10f;
+
+        }
+
+
+
+        scrollbar.size = strength / strengthMax;
+
+        
+       
     }
+
+
 
 }
 
--- Assets/Sebastien/Scripts/Death.cs
+++ Assets/Sebastien/Scripts/Death.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Death : MonoBehaviour
 {
+    public bool isDead = false;
+    public int livesLeft;
+
+    public GameObject lifeHUD1;
+    public GameObject lifeHUD2;
+    public GameObject lifeHUD3;
+
+    public GameObject player;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,15 +23,72 @@ public class Death : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (livesLeft == 3)
+        {
+            lifeHUD1.SetActive(true);
+            lifeHUD2.SetActive(true);
+            lifeHUD3.SetActive(true);
+        }
+        else if (livesLeft == 2)
+        {
+            lifeHUD1.SetActive(true);
+            lifeHUD2.SetActive(true);
+            lifeHUD3.SetActive(false);
+        }
+
+        else if (livesLeft == 1)
+        {
+            lifeHUD1.SetActive(true);
+            lifeHUD2.SetActive(false);
+            lifeHUD3.SetActive(false);
+        }
+
+        if (isDead)
+        {
+            LifeLoss();
+            Respawn();
+
+        }
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.gameObject.name == "Ball")
         {
+            collision.gameObject.SetActive(false);
+            isDead = true;
+            Debug.Log(livesLeft + " vies restantes");
+           
+        }
 
+    }
+
+    public void LifeLoss()
+    {
+
+        if (livesLeft == 3)
+        {
+            livesLeft = 2;
 
         }
+        else if (livesLeft == 2)
+        {
+            livesLeft = 1;
+        }
+
+        else if (livesLeft == 1)
+        {
+            livesLeft = 0;
+        }
+    }
+
+    public void Respawn()
+    {
+        if (isDead)
+        {
+            Instantiate(player, (new Vector2(-7,-1.5f)), transform.rotation);
+            isDead = false;
+        }
+
     }
 }
