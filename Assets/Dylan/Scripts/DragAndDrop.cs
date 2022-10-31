78cda49e545f52bd609e7961e96d3c96189ea3b4
GOARANT Dylan <dylan.goarant@gmail.com>
2022-10-31 14:50:51 +0100

    mise en place du drag and drop avec les flèches.

--- Assets/Dylan/Scripts/DragAndDrop.cs
+++ Assets/Dylan/Scripts/DragAndDrop.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class DragAndDrop : MonoBehaviour
 {
+    public float curseurMoveLeft;
+    public float curseurMoveRight;
+    public float curseurMoveUp = 1f;
+    public float curseurMoveDown;
+    public GameObject curseur;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = new Vector3(0f, curseur.transform.position.y + curseurMoveUp);
+            Debug.Log("A");
+        }
     }
 }
