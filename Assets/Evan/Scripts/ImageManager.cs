358a5ef34e99af43ef194f6f49bc63cb0ae69b5c
RENNES\e.trehin <e.trehin@rns@ecolescreatives.com>
2023-01-10 15:40:37 +0100

    zibou

--- Assets/Evan/Scripts/ImageManager.cs
+++ Assets/Evan/Scripts/ImageManager.cs
@@ -3,23 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ImageManager : MonoBehaviour
+namespace Evan
 {
-
-    public List<Button> images;
-    public Sprite dSprite;
-    public SpriteState ss;
-    // Start is called before the first frame update
-    void Start()
+    public class ImageManager : MonoBehaviour
     {
-        var buttonSelected = images[Random.Range(0, images.Count)];
-        buttonSelected.spriteState = ss;
-        (buttonSelected.targetGraphic as Image).sprite= dSprite;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+        public List<Button> images;
+        public Sprite dSprite;
+        public SpriteState ss;
+        // Start is called before the first frame update
+        void Start()
+        {
+            var buttonSelected = images[Random.Range(0, images.Count)];
+            buttonSelected.spriteState = ss;
+            (buttonSelected.targetGraphic as Image).sprite= dSprite;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
         
+        }
     }
+
 }
