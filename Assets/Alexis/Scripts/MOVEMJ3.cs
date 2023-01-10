13f794ab82d54af6a45274a17c900fb386a51275
Alexis <al.chevalier@rns.ecolescreatives.com>
2023-01-10 15:40:18 +0100

    MJ3 move

--- Assets/Alexis/Scripts/MOVEMJ3.cs
+++ Assets/Alexis/Scripts/MOVEMJ3.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class M : MonoBehaviour
+public class MOVEMJ3 : MonoBehaviour
 {
+    public List<button> ListeBouttons;
+    public Button CurrentButton = ListeBouttons[0];
+    private int index_boutton;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +20,23 @@ public class M : MonoBehaviour
     {
         
     }
+
+ void Change_Boutton_Haut()
+    {
+        while (index_boutton < 8) 
+        {
+            CurrentButton = ListeBouttons[index_boutton + 1];
+            CurrentButton.High
+                }
+        
+    }
+    void Change_Boutton_Down()
+    {
+        while (index_boutton > 0)
+        {
+            CurrentButton = ListeBouttons[index_boutton - 1];
+        }
+
+    }
+
 }
