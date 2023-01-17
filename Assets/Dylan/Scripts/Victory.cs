0ca0e821a5316cb249de295a4a3e1d35560ebb24
GOARANT Dylan <d.goarant@rns.ecolescreatives.com>
2023-01-17 13:19:04 +0100

    no message

--- Assets/Dylan/Scripts/Victory.cs
+++ Assets/Dylan/Scripts/Victory.cs
@@ -1,19 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Victory : MonoBehaviour
-
+namespace Dylan
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public class Victory : MonoBehaviour
 
-    // Update is called once per frame
-    void Update()
     {
-        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
