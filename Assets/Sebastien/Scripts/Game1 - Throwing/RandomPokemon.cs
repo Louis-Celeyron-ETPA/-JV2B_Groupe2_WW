2bf3fbb5dc14e7eb4f7a5730fd731ad7b18a3c05
Sébastien Salmon <s.salmon@rns.ecolescreatives.com>
2023-01-23 20:10:35 +0100

    Game 1 - Ajout des Fakemons terminé

--- Assets/Sebastien/Scripts/Game1 - Throwing/RandomPokemon.cs
+++ Assets/Sebastien/Scripts/Game1 - Throwing/RandomPokemon.cs
@@ -7,37 +7,50 @@ namespace SebastienMiniGame1
      public class RandomPokemon : MonoBehaviour
         {
             public GameObject pokemon;
+            public GameObject disposableGround; //Sol désactivé lorsque le Pokémon n'est pas sur la terre
             public SpriteRenderer pokemonDisplayed;
             public Sprite[] spriteArray;
             public string locationPokemon;
             public int nbDrawnPokemon;
+            
         
 
             void Awake()
             {
-                nbDrawnPokemon = Random.Range(0,152);
+                nbDrawnPokemon = Random.Range(0,10);
 
+
+               
             }
    
             // Start is called before the first frame update
             void Start()
             {
 
-            pokemonDisplayed.sprite = GetComponent<SpriteRenderer>().sprite = spriteArray[nbDrawnPokemon];
+            pokemonDisplayed.sprite = spriteArray[nbDrawnPokemon];
 
-                if (nbDrawnPokemon <= 2)
+                if (nbDrawnPokemon <= 4)
                 {
                     Debug.Log("il nage ce pokemon");
                     locationPokemon = "water";
                     PokemonPositioning();
+                    
                 }
 
-                else if (nbDrawnPokemon > 2)
+                else if (nbDrawnPokemon > 4 && nbDrawnPokemon <= 6)
                 {
                     Debug.Log("wow, ce pokemon vole");
                     locationPokemon = "fly";
                     PokemonPositioning();
                 }
+
+                else if (nbDrawnPokemon > 6)
+                {
+                    Debug.Log("Pas grand chose à dire, il est par terre");
+                    locationPokemon = "ground";
+                    PokemonPositioning();
+                }
+
                 
 
             }
@@ -54,11 +67,19 @@ namespace SebastienMiniGame1
                 if (locationPokemon == "water")
                 {
                     pokemon.transform.position = new Vector2(Random.Range(-5, 6), -4.2f);
+                    disposableGround.SetActive(false);
                 }
 
                 else if (locationPokemon == "fly")
                 {
                     pokemon.transform.position = new Vector2(Random.Range(-5, 6), 0f);
+                    disposableGround.SetActive(false);
+
+                }
+
+                else if (locationPokemon == "ground")
+                {
+                    pokemon.transform.position = new Vector2(1.75f, -2.75f);
 
                 }
 
